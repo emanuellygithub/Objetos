a83f704b8075def0c9cb5edac97b2634fb5cd9d4
Emanuelly Menezes Ayala <132668131+emanuellygithub@users.noreply.github.com>
2024-06-24 08:36:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+const perguntas = [
+    {
+    enunciado: "Pergunta 1" O aumento do desemprego devido a substituição de trabalhadores por máquinas já é evidente. O que você pensa sobre isso?
+    alternativas: [
+    "Alternativa 1" Futuramente irá faltar emprego
+    "Alternativa 2" Vai melhorar a eficiência e a produtividade no ambiente de trabalho
+    ]
+    },
+    {
+    enunciado: "Pergunta 2", Quais as preoupações éticas relacionadas ao uso da inteligência artificial?
+    alternativas: [
+    "Alternativa 1", Questionamento morais devido a desigualdade social
+    "Alternativa 2" Falta de transparência nas decisôes tomadas por algoritmos de IA
+    ]
+    },
+    {
+    enunciado: "Pergunta 3",
+    alternativas: [
+    "Alternativa 1",
+    "Alternativa 2"
+    ]
+    },
+    {
+    enunciado: "Pergunta 4",
+    alternativas: [
+    "Alternativa 1",
+    "Alternativa 2"
+    ]
+    },
+    {
+    enunciado: "Pergunta 5",
+    alternativas: [
+    "Alternativa 1",
+    "Alternativa 2"
+    ]
+    }
+    ];
